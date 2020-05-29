b96c9967e5db75124e967409399f03e78077d51d
qinhao <631256435@qq.com>
2020-05-29 14:25:24 +0800

    初始化

---
+++ src/VideoUrlParser.php
@@ -0,0 +1,17 @@
+<?php
+namespace collectVideo;
+/**
+ * Created by PhpStorm.
+ * User: 秦豪
+ * Date: 2020/5/29
+ * Time: 14:15
+ */
+
+class VideoUrlParser
+{
+
+    public static function test(){
+        echo '1qweqw';
+    }
+
+}
---
+++ test/TestVideoUrlParser.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 秦豪
+ * Date: 2020/5/29
+ * Time: 14:17
+ */
+
+use collectVideo\VideoUrlParser;
+
+require_once '../vendor/autoload.php';
+
+VideoUrlParser::test();
